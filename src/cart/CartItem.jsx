0cc771a669e8e2ffe1bac975fd8ef1137334b393
ervinmonterroso04@gmail.com <ervinmonterroso04@gmail.com>
2026-09-16 21:58:47 -0400

    src

---
+++ src/cart/CartItem.jsx
@@ -0,0 +1,17 @@
+import CartItemQuantity from "./CartitemQuantity";
+
+export default function CartItem({ item, removeFromCart, addToCart }) {
+  return (
+    <li className="cart-item">
+      <div>
+        {item.image}
+        {item.name}
+      </div>
+      <CartItemQuantity
+        item={item}
+        removeFromCart={removeFromCart}
+        addToCart={addToCart}
+      />
+    </li>
+  );
+}
